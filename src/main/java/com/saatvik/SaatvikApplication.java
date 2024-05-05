e10d4c403d7e6371053e2362b2816ef9b90f2a0d
birudeogarande <birudeogarande@gmaill.com>
2024-05-05 08:06:56 +0530

    added initial commit

---
+++ src/main/java/com/saatvik/SaatvikApplication.java
@@ -0,0 +1,13 @@
+package com.saatvik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaatvikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaatvikApplication.class, args);
+	}
+
+}
